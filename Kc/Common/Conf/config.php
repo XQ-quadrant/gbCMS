640be150adc621e23664ac1260ede52829528941
XQ <quadrantone@outlook.com>
2016-02-06 18:17:03 +0800

    ........

--- Kc/Common/Conf/config.php
+++ Kc/Common/Conf/config.php
@@ -4,8 +4,8 @@ return array(
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'kc',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'zxqdhd',          // 密码
+    'DB_USER'               =>  'gb',      // 用户名
+    'DB_PWD'                =>  'gb',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
 );
